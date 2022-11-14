f0e5b0edd0de832eb56a5d690674bdf32b5aa25d
rutikthakre <rutikthakre@gmail.com>
2022-11-14 23:17:36 +0530

    port 8080

--- main.go
+++ main.go
@@ -52,5 +52,5 @@ func main() {
 	app.Delete("/deleteimage", delete.DeleteImage)   //**
 	app.Put("/updatefolder", update.UpdateFolder)
 	app.Get("/getfolder", read.GetClientFolder)
-	app.Listen(":80")
+	app.Listen(":8080")
 }
