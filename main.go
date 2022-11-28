be336e94ab5f0d348d01eff245be76bfc03826ec
rutikthakre <rutikthakre@gmail.com>
2022-11-29 01:39:55 +0530

    default cors

--- main.go
+++ main.go
@@ -15,10 +15,7 @@ import (
 	"fmt"
 	"log"
 	"os"
-
-	
 	"github.com/joho/godotenv"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/monitor"
 	"github.com/gofiber/fiber/v2/middleware/cors"
